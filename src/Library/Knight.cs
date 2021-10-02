8471d4ee2863f890e4fa92a1318d8d45226c29ac
[Sebastian Fripp] <[sebastian.fripp@correo.ucu.edu.uy]>
2021-10-02 11:01:49 -0300

    Agregado de clase Hero, Goblin y Bandit

--- src/Library/Knight.cs
+++ src/Library/Knight.cs
@@ -7,7 +7,7 @@ namespace Library
     /// Creacion de la clase Knight, que contiene 3 atributos para sus estadisticas, 1 para el nombre y 1 lista que representa su
     /// inventario.
     /// </summary>
-    public class Knight : Character
+    public class Knight : Hero
     {
         /// <summary>
         /// Constructor de la clase Knight
@@ -16,6 +16,7 @@ namespace Library
         public Knight(string name)
         {
             this.name = name;
+            this.victoryPointsCounter = 0;
             this.health = 100;
             this.attack = 25;
             this.armor = 35;
